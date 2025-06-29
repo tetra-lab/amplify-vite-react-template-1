2891a4aad64cb79e33c5ba95879d97875062f84a
Kaito Suzuki <contact.kaisuzu@gmail.com>
2025-06-29 18:17:16 +0900

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
-import { Amplify } from "aws-amplify";
-import outputs from "../amplify_outputs.json";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
 
-Amplify.configure(outputs);
+// --- ★★★ ここからが重要な追加部分です ★★★ ---
+import { Amplify } from 'aws-amplify'
+import amplifyconfig from './amplifyconfiguration.json'
+Amplify.configure(amplifyconfig)
+// --- ★★★ ここまで追加 ★★★ ---
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
-);
+  </React.StrictMode>,
+)
